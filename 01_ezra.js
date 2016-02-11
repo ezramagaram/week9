032d63a0dffb0b443aa93d598e48b650466a963f
Ezra Magaram <ezramagaram@seattleacademy.org>
2016-02-11 09:55:41 -0800

    add 01ezra.ja

--- 01_ezra.js
+++ 01_ezra.js
@@ -1,3 +1,3 @@
-//console.log(process.argv[2]);
-
-console.log('hello ' + process.argv[2]);
+secondWord = process.argv[2];
+thirdWord = process.argv[3];
+console.log('My name is ' + secondWord + ' and I am ' + thirdWord + " years old");
